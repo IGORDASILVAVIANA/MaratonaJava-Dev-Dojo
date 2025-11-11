cbeaa0041c805392f199bd4231925f5fc2ad7669
IgorVianaDev <igorviana.dev@gmail.com>
2025-11-10 21:00:47 -0300

    atualizações

--- src/NPolimorfismo/test/ProdutoTest01.java
+++ src/NPolimorfismo/test/ProdutoTest01.java
@@ -2,9 +2,14 @@ package NPolimorfismo.test;
 
 import NPolimorfismo.dominio.Computador;
 import NPolimorfismo.dominio.Tomate;
-
+import NPolimorfismo.servico.CalculadoraImposto;
 
 public class ProdutoTest01 {
-    Computador computador = new Computador("NUC10i7", 11000);
-    Tomate tomate = new Tomate("Tomate Italiano", 10);
+    public static void main(String[] args) {
+        Computador computador = new Computador("NUC10i7", 11000);
+        Tomate tomate = new Tomate("Tomate Italiano", 10);
+
+        CalculadoraImposto.calcularImpostoComputador(computador);
+        CalculadoraImposto.calcularImpostoTomate(tomate);
+    }
 }
